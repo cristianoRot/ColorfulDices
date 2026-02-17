4cd7af298d8cb063b64b33bd9bf2d7a8398fb819
matteodandrade <m.dandrade@campus.unimib.it>
2026-02-17 23:15:24 +0100

    Enhance video processing with dynamic visualization
    Added dynamic visualization for detected launches from video.

--- change_detection/main.m
+++ change_detection/main.m
@@ -1,11 +1,32 @@
-clear; clc;
+clear; clc; close all;
 
 filename = 'VIDEO-01.mp4'; 
-% ---------------------------------------
 
 % Controlla se il file esiste prima di lanciare la funzione
 if exist(filename, 'file')
-    process_video(filename);
+    % Analisi Video
+    ImmaginiLanci = process_video(filename);
+    num_lanci = length(ImmaginiLanci);
+    
+    disp(['Ho trovato ', num2str(num_lanci), ' lanci.']);
+    
+    % Visualizzazione Dinamica in base a quanti lanci sono stati trovati
+    if num_lanci > 0
+        figure('Name', 'Risultati Rilevamento', 'NumberTitle', 'off');
+        
+        % Calcolo automatico righe/colonne 
+        cols = ceil(sqrt(num_lanci)); 
+        rows = ceil(num_lanci / cols);
+        
+        for i = 1:num_lanci
+            subplot(rows, cols, i); 
+            imshow(ImmaginiLanci{i});
+            title(['Lancio ', num2str(i)]);
+        end
+    else
+        disp('Nessun lancio rilevato nel video.');
+    end
+    
 else
-    error('File video "%s" non trovato! Controlla il nome o la cartella.', filename);
+    error('File video non trovato nella cartella corrente.');
 end
